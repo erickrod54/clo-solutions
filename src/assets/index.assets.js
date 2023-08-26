04d220ef413677c886752ff34c43a8bc4d00a3b2
Erick Rodriguez <erickrod54@gmail.com>
2023-08-26 12:55:06 -0400

    Adding july 31 - 23 assets

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -23,6 +23,25 @@ import kitchencabinetsafter from './organization/kitchen-cabinets-after.JPG';
 import wardrobebefore from './organization/wardrobe-before.jpg';
 import wardrobeafter from './organization/wardrobe-after.jpg';
 
+/**organization assets july 31 - 2023*/
+import cabinetafter1 from './organization/kitchen-drawer-after.jpg';
+import cabinetbefore1 from './organization/cabinet_before1.jpg'
+import closetafter2 from './organization/closet_after2.jpg';
+import closetbefore2 from './organization/closet_before2.jpg';
+import drawerafter3 from './organization/drawer_after3.jpg';
+import drawerbefore3 from './organization/drawer_before3.jpg';
+import roomafter4 from './organization/room_after4.jpg';
+import roombefore4 from './organization/room_before4.jpg'
+import shelfbefore5 from './organization/shelf_before5.jpg';
+import shelfafter5 from './organization/shelf_after5.jpg';
+import studiobefore6 from './organization/studio_before6.jpg';
+import studioafter6 from './organization/studio_after6.jpg';
+
+
+
 export { logocolor, testbkg1, testbkg2, testbkg3, testbkg4, testbkg5, testbkg6, consultationbkg, checkmark, closetheader, closetheaderblue, consultationbkgremove }
 
 export { closetbefore, closetafter, hamperbefore, hamperafter, kitchendrawerbefore, kitchendrawerafter, kitchencabinetsbefore, kitchencabinetsafter, wardrobebefore, wardrobeafter}
+
+export {cabinetafter1, cabinetbefore1, closetafter2, closetbefore2, drawerafter3, drawerbefore3, roomafter4, roombefore4, shelfbefore5, shelfafter5, studiobefore6, studioafter6 }  
+               
